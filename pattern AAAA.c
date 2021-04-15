628cf25ed112038a77d257a83816619a6f21b9d5
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2021-04-16 00:36:08 +0530

    Add files via upload

---
+++ pattern AAAA.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+     int i,j,num1,num2,x=65;
+
+     printf("Program to print pattern \n");
+
+     printf("How many rows do you want to print:-  \n");
+     scanf("%d",&num1);
+
+     printf("How many columns do you want to print.:-  \n");
+     scanf("%d",&num2);
+
+     for(i=0;i<num1;i++)
+     {
+         printf("\n");
+         for(j=0;j<num2;j++)
+         {
+             printf("\t %c",x);
+         }
+         x++;
+     }
+
+
+
+
+
+
+}
