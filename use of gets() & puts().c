5630838bc11081700a10e90f19a1469ea060b421
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2021-05-03 00:24:25 +0530

    Add files via upload

---
+++ use of gets() & puts().c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+main()
+{
+    char name[20];
+
+    printf("Program to print your name using get & put function\n");
+    gets(name);
+    puts(name);
+
+    getch();
+
+    return 0;
+}
