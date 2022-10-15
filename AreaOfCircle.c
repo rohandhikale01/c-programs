3d071b6ee3374d45fed9daccf7a153632d2074b2
Dhikale Rohan Rajendra <80779291+rohandhikale01@users.noreply.github.com>
2022-10-15 23:30:26 +0530

    Add files via upload

---
+++ AreaOfCircle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+main()
+{
+
+    float r,a,c;
+    printf("program to find area & circumference of circle");
+    printf("\n Enter radius of circle: ");
+    scanf("%f",&r);
+    a=(float)(22.0/7)*r*r;
+    c=2*(22.0/7)*r;
+    printf("Area=%f & circumference=%f",a,c);
+    getch();
+
+    //welcome to 
+
+}
